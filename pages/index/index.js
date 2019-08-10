9737f4267b393a1be638f7ac3515377c2788f2b1
流星Aday <1115876590@qq.com>
2019-08-10 16:49:33 +0800

    初始化

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,16 @@
 //index.js
+import {Api} from '../../utils/util';
 //获取应用实例
 const app = getApp()
 
 Page({
     data: {
         scrollTopOn: 'none',
-        scrollTopNum: 587,
+        scrollTopNum: 410,
+        list: [],
+        num: 10,
+        open: true,
+        pageId: 1
     },
     //监听滚动条
     onPageScroll: function ({scrollTop}) {
@@ -13,16 +18,47 @@ Page({
             this.setData({
                 scrollTopOn: 'none'
             });
-        } else if (scrollTop >= this.data.scrollTopNum) {
+        } else if (scrollTop >= this.data.scrollTopNum - 10) {
             this.data.scrollTopNum = scrollTop;
+            console.log(this.data.scrollTopNum)
             this.setData({
                 scrollTopOn: 'block'
             });
+            //懒加载商品列表
+            if (this.data.open === true) {
+                this.data.open = false;
+                //获取商品列表
+                let res = Api(app.globalData.urlApi + 'get-goods-list?userId=' + app.globalData.userId + '&pageId=' + this.data.pageId + '&pageSize=' + this.data.num + '&key=' + app.globalData.key + '&tmall=1', 'get');
+                res.then((resolve, reject) => {
+                    if (resolve.code !== -1) {
+                        this.setData({
+                            list: this.data.list.concat(resolve.data.list)
+                        })
+                        console.log(this.data.list)
+                        this.data.open = true;
+                        this.data.pageId += 1;
+                    } else {
+                        console.log(reject)
+                    }
+                })
+            }
+
         }
     },
     goTop() {
         wx.pageScrollTo({
             scrollTop: 0
         });
+    },
+    onLoad(query) {
+        // console.log(app.globalData.secretKey);
+
+    },
+    favorable({currentTarget}) {
+        wx.navigateTo({
+            url: '/ps/index/dads',
+            events: currentTarget.dataset
+        });
+        console.log();
     }
 })
--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,39 @@
 const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
 const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+    n = n.toString()
+    return n[1] ? n : '0' + n
+}
+
+const Api = (url, method, data = []) => {
+    return new Promise((resolve, reject) => {
+        wx.request({
+            url: url,
+            method: method,
+            data: data,
+            success({data}) {
+                if (data.code === 0) {
+                    return resolve(data);
+                }
+                return resolve(-1);
+            },
+            fail(err) {
+                return reject(err);
+            }
+        });
+    });
 }
 
 module.exports = {
-  formatTime: formatTime
+    formatTime: formatTime,
+    Api
 }
