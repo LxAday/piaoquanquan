48d2b2702212b43e70d4b3e05d2c6dfff4ae93fe
流星Aday <1115876590@qq.com>
2019-08-15 21:31:44 +0800

    主页

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,20 +11,23 @@ Page({
         num: 10,
         open: true,
         pageId: 1,
-        seniority: []
+        seniority: [],
+        imgs: [],
+        hot: [],
+        classify: []
     },
     //监听滚动条
     onPageScroll: function ({scrollTop}) {
-        if (scrollTop <= this.data.scrollTopNum / 2) {
+        if (scrollTop < 300) {
             this.setData({
                 scrollTopOn: 'none'
             });
-        } else if (scrollTop >= this.data.scrollTopNum - 10) {
-            this.data.scrollTopNum = scrollTop;
-            console.log(this.data.scrollTopNum)
+        } else {
             this.setData({
                 scrollTopOn: 'block'
             });
+        }
+        if (scrollTop > this.data.scrollTopNum) {
             //懒加载商品列表
             if (this.data.open === true) {
                 this.data.open = false;
@@ -37,9 +40,9 @@ Page({
                             this.setData({
                                 list: this.data.list.concat(resolve.data.list)
                             })
-                            console.log(this.data.list)
                             this.data.open = true;
                             this.data.pageId += 1;
+                            this.data.scrollTopNum = scrollTop + 900;
                         } else {
                             console.log(reject)
                             wx.showToast({
@@ -61,29 +64,57 @@ Page({
         });
     },
     onLoad(query) {
-        //判断key
-        if (app.globalData.key && app.globalData.key !== '') {
-            funcs.get_ranking_list(this);
+        if (wx.getStorageSync('get_ranking_list') && wx.getStorageSync('get_ranking_list') !== '') {
+            this.setData({
+                seniority: wx.getStorageSync('get_ranking_list')
+            })
         } else {
-            //调用app的keyCallback属性
-            app.keyCallback = key => {
-                //判断key
-                if (key !== '') {
-                    funcs.get_ranking_list(this);
+            //判断key
+            if (app.globalData.key && app.globalData.key !== '') {
+                funcs.get_ranking_list(this);
+            } else {
+                //调用app的keyCallback属性
+                app.keyCallback = key => {
+                    //判断key
+                    if (key !== '') {
+                        funcs.top_search(this, 'hot', 'recommended-column')
+                        funcs.top_search(this, 'classify', 'classification-of-icon')
+                        funcs.get_ranking_list(this, 'imgs', 1, '', 5)
+                        funcs.get_ranking_list(this, 'seniority', 3, '')
+                    }
                 }
             }
         }
 
+
         //定义页面方法集合
         let funcs = {
-            get_ranking_list(event) {
-                let res = Api(app.globalData.urlApi + 'get-ranking-list?userId=' + app.globalData.userId + '&key=' + app.globalData.key, 'get');
+            get_ranking_list(event, objs, num = 1, cid = '', nu = 0) {
+                let res = Api(app.globalData.urlApi + 'get-ranking-list?userId=' + app.globalData.userId + '&key=' + app.globalData.key + '&rankType=' + num + '&cid=' + cid + '&nu=' + nu);
                 res.then((resolve, reject) => {
                     if (resolve.code !== -1) {
-                        event.setData({
-                            seniority: resolve.data
+                        let obj = {};
+                        obj[objs] = resolve.data
+                        event.setData(obj)
+                    } else {
+                        console.log(reject)
+                        wx.showToast({
+                            title: '加载失败',
+                            icon: 'none',
+                            duration: 1000,
+                            mask: true
                         })
-                        console.log(event.data.seniority)
+                    }
+                })
+            },
+            //推荐栏目   分类图标
+            top_search(event, objs, url) {
+                let res = Api(app.globalData.urlApi + url + '?userId=' + app.globalData.userId + '&key=' + app.globalData.key);
+                res.then((resolve, reject) => {
+                    if (resolve.code !== -1) {
+                        let obj = {};
+                        obj[objs] = resolve.data
+                        event.setData(obj)
                     } else {
                         console.log(reject)
                         wx.showToast({
@@ -95,7 +126,6 @@ Page({
                     }
                 })
             }
-
         }
 
     },
