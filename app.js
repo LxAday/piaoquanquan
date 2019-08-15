24c99eef9d8b28b588d278718737411b04a5e4c5
流星Aday <1115876590@qq.com>
2019-08-15 21:31:29 +0800

    app配置

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ import {Api} from '/utils/util';
 
 App({
     data: {
-        urlTop: 'http://la.com/',
+        urlTop: 'https://wx.lx-blog.cn/',
         //授权秘钥
         secretKey: 'fe8684fa9acc2bbb00f5be238962b390d9b30a18',
     },
     globalData: {
         userInfo: null,
         key: '',
-        urlApi: 'http://la.com/api/',
+        urlApi: 'https://wx.lx-blog.cn/api/',
         userId: 'a'
     },
     onLaunch: function () {
@@ -36,37 +36,44 @@ App({
                         success: res => {
                             // 可以将 res 发送给后台解码出 unionId
                             this.globalData.userInfo = res.userInfo
-                            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-                            // 所以此处加入 callback 以防止这种情况
-                            if (this.userInfoReadyCallback) {
-                                this.userInfoReadyCallback(res)
-                            }
+                            this.globalData.userId = res.signature
+                            //接口授权
+                            let ress = Api(this.data.urlTop + 'accredit?secretKey=' + this.data.secretKey + '&userId=' + this.globalData.userId, 'get');
+                            ress.then((resolve, reject) => {
+                                if (resolve.code !== -1) {
+                                    this.globalData.key = resolve.data.key
+                                    // 由于 Api 是网络请求，可能会在 Page.onLoad 之后才返回
+                                    // 所以此处加入 callback 以防止这种情况
+                                    if (this.keyCallback) {
+                                        this.keyCallback(resolve.data.key)
+                                    }
+                                } else {
+                                    console.log(reject)
+                                    wx.showToast({
+                                        title: '加载失败',
+                                        icon: 'none',
+                                        duration: 1000,
+                                        mask: true
+                                    })
+
+                                }
+                            })
+
+                        },
+                        fail(res) {
+                            console.log(res)
+                            wx.showToast({
+                                title: '加载失败',
+                                icon: 'none',
+                                duration: 1000,
+                                mask: true
+                            })
                         }
                     })
                 }
             }
         });
 
-        //接口授权
-        let res = Api(this.data.urlTop + 'accredit?secretKey=' + this.data.secretKey + '&userId=' + this.globalData.userId, 'get');
-        res.then((resolve, reject) => {
-            if (resolve.code !== -1) {
-                this.globalData.key = resolve.data.key
-                // 由于 Api 是网络请求，可能会在 Page.onLoad 之后才返回
-                // 所以此处加入 callback 以防止这种情况
-                if (this.keyCallback) {
-                    this.keyCallback(resolve.data.key)
-                }
-            } else {
-                console.log(reject)
-                wx.showToast({
-                    title: '加载失败',
-                    icon: 'none',
-                    duration: 1000,
-                    mask:true
-                })
 
-            }
-        })
     }
 })
